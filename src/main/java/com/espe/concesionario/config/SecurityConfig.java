ab4ef1eb7a6ec02ddce81b0e8db1b44f0b24a13f
Ronald Ramírez <reramirez1@espe.edu.ec>
2023-03-09 23:01:54 -0500

    update login

--- src/main/java/com/espe/concesionario/config/SecurityConfig.java
+++ src/main/java/com/espe/concesionario/config/SecurityConfig.java
@@ -1,43 +1,52 @@
 package com.espe.concesionario.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity(debug = false)
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors().disable()
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeHttpRequests((authorize) -> authorize
-                        .antMatchers("/user").hasAuthority("ROLE_USER")
-                        .antMatchers("/api/venta").hasAuthority("ROLE_ADMIN")
-                        .antMatchers("/moderator").hasAuthority("ROLE_MODERATOR")
-                        .antMatchers("/").permitAll()
-                        .anyRequest().denyAll()
-                )
-                .httpBasic()
-                .and()
-                .formLogin();
-        return http.build();
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/login").permitAll()
+                .anyRequest().authenticated().and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
-    MyUsersDetailsService myUsersDetailsService() {
-        return new MyUsersDetailsService();
-    }
-
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
